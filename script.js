4a907ef4ea761f4c595feecb4a07ffdcbc076023
Laurie Lonecrow <laurieteresa@gmail.com>
2021-02-20 15:39:34 -0500

    Added Title-logo, load screen, and adjusted responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+gsap.registerPlugin(ScrollTrigger);
+console.clear();
+
+gsap.fromTo(".layer",
+{transform: "scale(.1)", opacity:0},
+{transform: "scale(1.2)", opacity:1, duration: 8, ease: "bounce"});
+gsap.fromTo(".baselayer",
+{opacity:0, transform: "scale(.1)"},
+{opacity:1, transform: "scale(1.2)", duration: 8, ease: "bounce"});
+gsap.fromTo(".main-nav",
+{opacity:0, transform: "scale(.1)"},
+{opacity:1, transform: "scale(1.2)", delay: 8, duration: 1});
+
+
+//gsap.fromTo(".bright",
+  //{webkitFilter: "brightness(1)", filter: "brightness(1)" },
+  //{
+  //  duration: .25,
+//     webkitFilter: "brightness(2)",
+//     filter: "brightness(2)",
+//     yoyo: true,
+//     repeat: -1,
+//     repeatDelay: 0.5,
+//     ease: "none",
+//     stagger: .1,
+//   }
+// );
+
+//gsap.fromTo(".hue",
+// {webkitFilter: "hue-rotate(0deg)", filter: "hue-rotate(0deg)"},
+// {
+// duration: .5,
+// webkitFilter: "hue-rotate(90deg)",
+// filter: "hue-rotate(90deg)",
+// yoyo: true,
+// repeat: -1,
+// ease: "none",
+// stagger: .5,
+// });
+
+var tl = gsap.timeline({
+  scrollTrigger:{
+  trigger: ".main-nav",
+  start: "top top",
+  scrub: 1,
+  pin: true,
+  pinSpacing: false,
+}});
+
+tl.fromTo(".wagon", {x:900}, {x:-50, duration: 25});
+
+tl.fromTo(".pContent", {yPercent: 0},{
+  yPercent: -1, ease: "circ"});
+
+
+
+const imgs = gsap.utils.toArray(".skewElem");
+imgs.forEach(img => {gsap.to(img,
+  {scrollTrigger: {
+    trigger: img,
+  scrub: 1
+},
+    transform: "scale(1.1)"},
+  )});
+
+const clouds = gsap.utils.toArray(".cloud");
+clouds.forEach(cloud => {gsap.to(cloud,
+  {scrollTrigger:{
+    trigger: ".cloud-1",
+    scrub: 1
+  },
+    transform: "translate(20px, -200px)"},
+)});
+
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
@@ -5,69 +79,6 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.fromTo(".layer",
-{transform: "scale(.1)", opacity:0},
-{transform: "scale(1.2)", opacity:1, duration: 2, ease: "bounce"});
-gsap.fromTo(".baselayer",
-{opacity:0, transform: "scale(.1)"},
-{opacity:1, transform: "scale(1.2)", duration: 2, ease: "bounce"});
-gsap.fromTo(".main-nav",
-{opacity:0, transform: "scale(.1)"},
-{opacity:1, transform: "scale(1.2)", delay: 2, duration: 1});
-
-
-gsap.fromTo(".bright",
-  {webkitFilter: "brightness(1)", filter: "brightness(1)" },
-  {
-    duration: .25,
-    webkitFilter: "brightness(2)",
-    filter: "brightness(2)",
-    yoyo: true,
-    repeat: -1,
-    repeatDelay: 0.5,
-    ease: "none",
-    stagger: .1,
-  }
-);
-
-gsap.fromTo(".hue",
-{webkitFilter: "hue-rotate(0deg)", filter: "hue-rotate(0deg)"},
-{
-duration: .5,
-webkitFilter: "hue-rotate(90deg)",
-filter: "hue-rotate(90deg)",
-yoyo: true,
-repeat: -1,
-ease: "none",
-stagger: .5,
-});
-
-var tl = gsap.timeline({
-  scrollTrigger:{
-  trigger: ".pSection",
-  start: "top top",
-  scrub: 1,
-  pin: true,
-  pinSpacing: false,
-}});
-
-tl.fromTo(".pContent", {yPercent: 0},{
-  yPercent: -10, ease: "circ"});
-
-tl.fromTo(".wagon", {x:900}, {x:-900, duration: 5});
-
-const imgs = gsap.utils.toArray(".skewElem");
-imgs.forEach(img => {gsap.to(img,
-  {scrollTrigger: {
-    trigger: img,
-  scrub: 1},
-    transform: "scale(1.1)"},
-  )});
-
-
 //const layers = gsap.utils.toArray(".layer");
 //layers.forEach(layer => {
 //tl.fromTo(layer, {opacity: 1.0, transform: "scale(1)"},
