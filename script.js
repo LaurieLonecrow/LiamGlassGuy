09eadb7230653f144f95df3613da21ef06163672
Laurie Lonecrow <laurieteresa@gmail.com>
2021-03-21 14:49:24 -0400

    Final update to all cloud animations

--- script.js
+++ script.js
@@ -10,6 +10,61 @@ const overlay = function() {
 };
 
 
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".header-logo",
+    start: "top top",
+    scrub: 1,
+    pin: false,
+    pinSpacing: false,
+  }
+});
+
+tl.fromTo(".wagon", {
+  x: 900
+}, {
+  x: -50,
+});
+
+tl.fromTo(".cloud-1", {
+  transform: "translate(100px, 10px)",
+}, {
+  transform: "translate(-100px, -5px)",
+});
+
+tl.fromTo(".cloud-2", {
+  transform: "translate(500px, -10px)",
+}, {
+  transform: "translate(700px, -5px)",
+});
+// tl.fromTo(".pContent", {
+//   yPercent: 0
+// }, {
+//   yPercent: -1,
+//   ease: "circ"
+// });
+
+
+const clouds = gsap.utils.toArray(".cloud");
+clouds.forEach(cloud => {
+  gsap.to(cloud, {
+    scrollTrigger: {
+      scrub: 1
+    },
+    transform: "translate(50px, -100px)"
+  }, )
+});
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+//Load Image Animation
+
 // gsap.fromTo(".layer",
 // {transform: "scale(.1)", opacity:0},
 // {transform: "scale(1.2)", opacity:1, duration: 8, ease: "bounce"});
@@ -46,50 +101,6 @@ const overlay = function() {
 // ease: "none",
 // stagger: .5,
 // });
-
-var tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".header-logo",
-    start: "top top",
-    scrub: 1,
-    pin: false,
-    pinSpacing: false,
-  }
-});
-
-tl.fromTo(".wagon", {
-  x: 900
-}, {
-  x: -50,
-  duration: 25
-});
-
-tl.fromTo(".pContent", {
-  yPercent: 0
-}, {
-  yPercent: -1,
-  ease: "circ"
-});
-
-
-const clouds = gsap.utils.toArray(".cloud");
-clouds.forEach(cloud => {
-  gsap.to(cloud, {
-    scrollTrigger: {
-      trigger: ".cloud-2",
-      scrub: 1
-    },
-    transform: "translate(20px, -200px)"
-  }, )
-});
-
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
 
 
 //const layers = gsap.utils.toArray(".layer");
