b390f4815ae5306688e1270d2fdc1b386d241542
Laurie Lonecrow <laurieteresa@gmail.com>
2021-03-30 18:00:35 -0400

    Created slides function

--- modal.js
+++ modal.js
@@ -1,25 +1,31 @@
 'use strict';
 
-const glass = document.querySelectorAll('.col img');
-const pipes = document.getElementById('slide-show').alt = "pipes";
-const bongs = document.getElementById('slide-show').alt = "bongs";
-const newSlide = document.createElement('img');
-newSlide.className = ('slide-image')
-newSlide.src =
-  `images/glass/pipes/${"liamglass" + Math.floor(
-  (Math.random() * pipes.length) + 1)}.jpg`;
+const images = document.querySelectorAll('img')
 
+const glass = document.querySelectorAll('.col img');
+const pipes = document.querySelectorAll('.pipes');
+const bongs = document.getElementById('slide-show').alt = "bong";
+const slides = document.querySelectorAll('.new-slide');
+// document.createElement('img');
+// newSlide.className = ('slide-image')
+// newSlide.src =
+//   `images/glass/pipes/${"liamglass" + Math.floor(
+//   (Math.random() * pipes.length) + 1)}.jpg`;
+
+const appendSlide = function(slides) {
+  slides.forEach(function(slide, i) {
+    const newSlide = `<img class="slide-image" src="${src}" alt="pipes-are-tight">`
+    slides[i].insertAdjacentHTML('afterbegin', newSlide)
+  });
+}
 
 function openLightbox() {
   document.getElementById('lightbox').style.width = "100%"
-
   if (pipes) {
-    const slides = document.querySelectorAll('.new-slide')
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].append(newSlide)
-    };
+    appendSlide(slides, pipes)
   };
 };
+
 
 function closeLightbox() {
   document.getElementById('lightbox').style.width = "0%";
