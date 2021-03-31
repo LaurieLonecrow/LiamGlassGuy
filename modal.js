766b15492b75a97581080e8d142c88c3f475c834
Laurie Lonecrow <laurieteresa@gmail.com>
2021-03-31 16:46:25 -0400

    Created Slide Show Function

--- modal.js
+++ modal.js
@@ -1,26 +1,35 @@
 'use strict';
 
-const images = document.querySelectorAll('img')
-
 const glass = document.querySelectorAll('.col img');
-const pipes = document.querySelectorAll('.pipes');
+const pipes = document.getElementById('slide-show').alt = "pipes";
 const bongs = document.getElementById('slide-show').alt = "bong";
-const slides = document.querySelectorAll('.new-slide');
-// document.createElement('img');
-// newSlide.className = ('slide-image')
-// newSlide.src =
-//   `images/glass/pipes/${"liamglass" + Math.floor(
-//   (Math.random() * pipes.length) + 1)}.jpg`;
+const slides = document.getElementById('slides');
+const images = []; //images is an array of currentSrc for slide-show imgs
+const imageArr = function() {
+  const imgs = document.querySelectorAll('#slide-show')
+  imgs.forEach(function(img, i) {
+    const x = imgs[i].currentSrc
+    images.push(x)
+  });
+};
+
 
 const appendSlide = function(slides) {
-  slides.forEach(function(slide, i) {
-    const newSlide = `<img class="slide-image" src="${src}" alt="pipes-are-tight">`
-    slides[i].insertAdjacentHTML('afterbegin', newSlide)
-  });
-}
+  slides.innerHTML = "";
+  imageArr();
+  images.forEach(function(image, i) {
+    const src = images[i];
+    const newSlide = `<div id="" class="slide new-slide">
+      <img id="slideImage" class="slide-image" src="${src}" alt="pipes-are-tight">
+    </div>`;
+    slides.insertAdjacentHTML('afterbegin', newSlide)
+  })
+};
+
 
 function openLightbox() {
-  document.getElementById('lightbox').style.width = "100%"
+  document.getElementById('lightbox').style.width = "100%";
+
   if (pipes) {
     appendSlide(slides, pipes)
   };
