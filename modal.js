63f1c3b33824f9d0e0795d973c33906db52c2774
Laurie Lonecrow <laurieteresa@gmail.com>
2021-04-02 16:57:42 -0400

    Update to Slideshow function

--- modal.js
+++ modal.js
@@ -4,35 +4,27 @@ const glass = document.querySelectorAll('.col img');
 const pipes = document.getElementById('slide-show').alt = "pipes";
 const bongs = document.getElementById('slide-show').alt = "bong";
 const slides = document.getElementById('slides');
-const images = []; //images is an array of currentSrc for slide-show imgs
-const imageArr = function() {
-  const imgs = document.querySelectorAll('#slide-show')
+const imgs = document.querySelectorAll('#slide-show');
+
+
+function appendSlide() {
+  slides.innerHTML = "";
   imgs.forEach(function(img, i) {
-    const x = imgs[i].currentSrc
-    images.push(x)
+    const imgSrc = imgs[i].currentSrc;
+    const imgAlt = imgs[i].alt;
+    const newSlide = `<div id="" class="slide new-slide">
+      <img id="slideImage" class="slide-image" src=${imgSrc} alt=${imgAlt}>
+    </div>`;
+    slides.insertAdjacentHTML('afterbegin', newSlide)
   });
 };
 
 
-const appendSlide = function(slides) {
-  slides.innerHTML = "";
-  imageArr();
-  images.forEach(function(image, i) {
-    const src = images[i];
-    const newSlide = `<div id="" class="slide new-slide">
-      <img id="slideImage" class="slide-image" src="${src}" alt="pipes-are-tight">
-    </div>`;
-    slides.insertAdjacentHTML('afterbegin', newSlide)
-  })
-};
-
 
 function openLightbox() {
   document.getElementById('lightbox').style.width = "100%";
+  appendSlide();
 
-  if (pipes) {
-    appendSlide(slides, pipes)
-  };
 };
 
 
